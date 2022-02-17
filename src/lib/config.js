15fe638012e17e3d90d2293f86ece7237143b82e
ferwin <ferwin.arias@gmail.com>
2022-02-17 18:34:49 -0500

    others configd

--- src/lib/config.js
+++ src/lib/config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const config = {
-    port: process.env.PORT || 8080,
+    port: process.env.PORT,
     API_KEY: process.env.API_KEY,
     cors: process.env.CORS || 'http://localhost:3000',
     tableName: process.env.TABLE_NAME,
